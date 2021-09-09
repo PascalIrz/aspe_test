a4d224cf2662a51c4df6cd2453c6648c60a20a1f
PascalIrz <pascal.irz@gmail.com>
2021-09-09 14:43:26 +0200

    xx_operations_bgm___ : ajout commentaires

--- scripts/xx_operations_bgm_pour_oeb.R
+++ scripts/xx_operations_bgm_pour_oeb.R
@@ -1,8 +1,16 @@
+# Recherche des opérations qui ont été menées par BGM afin de transmettre les données à l'OEB
+
+# activation des packages
+library(tidyverse)
+
+# Chargement des données
 load(file = "processed_data/toutes_tables_aspe_sauf_mei.RData")
 
+# recherche des champs, ttes tables comprises, identifiant les intervenants et utilisateurs
 expl_trouver_variable("int_id")
 expl_trouver_variable("uti_id")
 
+# simplification des tables pour ne conserver que le nécessaire
 int_simp <- ref_intervenant %>%
   select(int_id,
          int_libelle_sandre)
@@ -18,7 +26,10 @@ ope_simp <- operation %>%
          ope_int_id_commanditaire,
          ope_int_id_validation_technique,
          ope_uti_id_creation,
-         ope_uti_id_derniere_modification) %>%
+         ope_uti_id_derniere_modification)
+
+# Jointure pour ajouter operateur, commanditaire etc. à la table ope_simp
+ope_simp <- ope_simp %>%
   left_join(y = int_simp %>%
               rename(ope_int_id_operateur_peche = int_id,
                      operateur_peche = int_libelle_sandre)) %>%
@@ -35,7 +46,8 @@ ope_simp <- operation %>%
               rename(ope_uti_id_derniere_modification = uti_id,
                      modificateur = uti_mail))
 
-
+# identification des opérations où soit on a "bgm" dans l'adresse mail de l'utilisateur
+# (2 champs, createur et modificateur), soit on a "B.G.M." dans l'intervenant (3 champs)
 ope_bgm <- ope_simp %>%
   mutate(bgm = case_when(
     str_detect(operateur_peche, pattern = "B.G.M.") ~ TRUE,
